bcd81a4a543af35ef45ec9b3811dfd1440f1aef6
Apurva <abadithe@caltech.edu>
2022-08-01 09:56:54 -0700

    problem_data

--- convex_concave_min_max_opt/problem_data.py
+++ convex_concave_min_max_opt/problem_data.py
@@ -10,6 +10,7 @@ import os
 import networkx as nx
 from components.road_network import RoadNetwork, create_network_from_file
 import matplotlib.pyplot as plt
+from scipy import sparse as sp
 
 debug = False
 
@@ -49,18 +50,77 @@ def objective(x,y,reg, edges_keys):
     # Objective function: c1.T x + c2.T y
     return c1, c2
 
-def feas_constraint(x,y):
-    pass
+def feas_constraint(x,y, edges_keys):
+    ne = len(list(edges_keys.keys())) # number of edges
+    Af1 = np.eye(ne)
+    Af2 = np.eye(ne)
+    Af3 = np.eye(ne)
+    blk_zeros = np.zeros((ne,ne))
 
-def cut_constraint(x,y):
-    pass
+    b_feas = np.zeros((3*ne,))
+    A_feas_f1= np.hstack((Af1, blk_zeros, blk_zeros, blk_zeros, blk_zeros))
+    A_feas_f2= np.hstack((blk_zeros, Af2, blk_zeros, blk_zeros, blk_zeros))
+    A_feas_f3= np.hstack((blk_zeros, blk_zeros, blk_zeros, blk_zeros, Af3))
+    A_feas = np.vstack((A_feas_f1, A_feas_f2, A_feas_f3))
+    return A_feas, b_feas
 
-def conservation_constraint(x,y):
-    pass
+def cut_constraint(x,y, edges_keys):
+    ne = len(list(edges_keys.keys())) # number of edges
+    Af1 = -1*np.eye(ne)
+    Af2 = -1*np.eye(ne)
+    Af3 = -1*np.eye(ne)
+    Ade = -1*np.eye(ne)
+    Aot = np.eye(ne)
+    blk_zeros = np.zeros((ne,ne))
 
-def capacity_constraint(x,y):
-    pass
-    
+    b_feas = np.zeros((3*ne,))
+    A_feas_f1= np.hstack((Af1, blk_zeros, Ade, Aot, blk_zeros))
+    A_feas_f2= np.hstack((blk_zeros, Af2, Ade, Aot, blk_zeros))
+    A_feas_f3= np.hstack((blk_zeros, blk_zeros, Ade, Aot, Af3))
+    A_feas = np.vstack((A_feas_f1, A_feas_f2, A_feas_f3))
+    return A_feas, b_feas
+
+def conservation_constraint(x,y, nodes_keys, edges_keys, src, int, sink):
+    for k,node in nodes_keys.items():
+        if node not in set(src, int, sink):
+            # Afeas1: sum_i f^i(u,v) >= sum_i f^i(v,u)
+            
+            # Afeas2: sum_i f^i(u,v) <= sum_i f^i(v,u)
+
+    Afeas = np.vstack((Afeas1, Afeas2))
+    bfeas = np.vstack((bfeas1, bfeas2))
+    return Afeas, bfeas
+
+def min_flow_constraint(x,y,edges_keys, src, int,sink):
+    ne = len(list(edges_keys.keys())) # number of edges
+    out_s1_edge_ind = [k for k, v in edges_keys.items() if v[0]==src]
+    out_s2_edge_ind = [k for k, v in edges_keys.items() if v[0]==int]
+    out_s3_edge_ind = [k for k, v in edges_keys.items() if v[0]==src]
+
+    zero_row_vec = np.zeros((1,ne))
+    af1_ones = np.ones((len(ne),))
+    af1 = sp.csr_array(af1_ones, (zero_row_vec, out_s1_edge_ind), shape=(1,ne))
+    af2 = sp.csr_array(af2_ones, (zero_row_vec, out_s2_edge_ind), shape=(1,ne))
+    af3 = sp.csr_array(af3_ones, (zero_row_vec, out_s3_edge_ind), shape=(1,ne))
+    bfeas = np.array([[1],[1],[1]])
+    Afeas = np.vstack((af1, af2, af3))
+    return Afeas, bfeas
+
+def capacity_constraint(x,y, edges_keys):
+    ne = len(list(edges_keys.keys())) # number of edges
+    Af1 = -1*np.eye(ne)
+    Af2 = -1*np.eye(ne)
+    Af3 = -1*np.eye(ne)
+    Aot = np.eye(ne)
+    blk_zeros = np.zeros((ne,ne))
+
+    b_feas = np.zeros((3*ne,))
+    A_feas_f1= np.hstack((Af1, blk_zeros, blk_zeros, Aot, blk_zeros))
+    A_feas_f2= np.hstack((blk_zeros, Af2, blk_zeros, Aot, blk_zeros))
+    A_feas_f3= np.hstack((blk_zeros, blk_zeros, blk_zeros, Aot, Af3))
+    A_feas = np.vstack((A_feas_f1, A_feas_f2, A_feas_f3))
+    return A_feas, b_feas
+
 if __name__ == '__main__':
     # test
     grid = "small"
