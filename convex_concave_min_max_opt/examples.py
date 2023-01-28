1be0f53a14bdc93caa00df816343c2b75c14166e
Apurva <abadithe@caltech.edu>
2023-01-27 22:06:55 -0800

    latest

--- convex_concave_min_max_opt/examples.py
+++ convex_concave_min_max_opt/examples.py
@@ -7,51 +7,59 @@ class ToyProb():
     def __init__(self, x0=None, y0=None, lam0 = None):
         if x0 is None:
             self.x0 = 0
+        else:
+            self.x0 = x0
         if y0 is None:
             self.y0 = 0
-        self.lam0 = None # Change
+        else:
+            self.y0 = y0
+
+        if lam0 is None:
+            set_init()
+        else:
+            self.lam0 = lam0
+
         self.T = 1000
-        self.K = 10
         self.alpha = 0.5
         self.N_inner = 5
         self.A = None
         self.B = None
         self.c = None
-        self.set_init()
         self.set_params()
         self.set_problem_data()
 
     def set_init(self):
-        self.lam0 = None # Change
+        self.lam0 = np.array([[0],[0]], dtype=object,) # Change
 
     def set_params(self):
-        self.T = 1000
-        self.K = 10
+        self.T = 100
         self.alpha = 0.5
         self.N_inner = 5
 
     def set_problem_data(self):
-        self.A = 1
-        self.B = -1
-        self.c = 1
+        self.A = np.array([[1],[-1]], dtype=object,)
+        self.B = np.array([[-1],[1]], dtype=object,)
+        self.c = np.array([[1],[-1]], dtype=object,)
 
     def define(self):
         init = [self.x0, self.y0, self.lam0]
-        params = [self.T, self.K, self.alpha, self.N_inner]
+        params = [self.T, self.alpha, self.N_inner]
         problem_data = [self.A, self.B, self.c]
         return init, params, problem_data
 
     def gradf_x(self, x, y):
         return x+y
 
-    def gradg_y(self, x, y):
+    def gradf_y(self, x, y):
         return x-y
 
     def projX(self, x):
         lbx = -1
         ubx = 1
-        k = len(x)
-        xproj = cp.Variable(k)
+        if isinstance(x, (list, tuple, np.ndarray)):
+            xproj = cp.Variable(x.shape)
+        else:
+            xproj = cp.Variable()
         l2_norm =  cp.sum_squares(x - xproj)
         constraints = [xproj <= ubx, xproj >=lbx]
         prob = cp.Problem(cp.Minimize(l2_norm), constraints)
@@ -65,8 +73,10 @@ class ToyProb():
     def projY(self, y):
         lby = -2
         uby = 0
-        k = len(y)
-        yproj = cp.Variable(k)
+        if isinstance(y, (list, tuple, np.ndarray)):
+            yproj = cp.Variable(y.shape)
+        else:
+            yproj = cp.Variable()
         l2_norm =  cp.sum_squares(y- yproj)
         constraints = [yproj <= uby, yproj >=lby]
         prob = cp.Problem(cp.Minimize(l2_norm), constraints)
@@ -78,9 +88,13 @@ class ToyProb():
              return yproj.value
 
     def proj_pos(self, vec):
-        k = len(vec)
-        lb = np.zeros((k,1))
-        vec_pos = cp.Variable(k) # Solves for -x --> ???
+        if isinstance(vec, (list, tuple, np.ndarray)):
+            lb = np.zeros(vec.shape)
+            vec_pos = cp.Variable(vec.shape)
+        else:
+            vec_pos = cp.Variable()
+            lb = 0
+
         l2_norm =  cp.sum_squares(vec_pos - vec)
         constraints = [vec_pos >=lb]
         prob = cp.Problem(cp.Minimize(l2_norm), constraints)
--- convex_concave_min_max_opt/mgd.py
+++ convex_concave_min_max_opt/mgd.py
@@ -3,19 +3,20 @@
 import numpy as np
 import pdb
 from examples import ToyProb
+import matplotlib.pyplot as plt
 
 def mgd(problem, init, params, problem_data):
     '''
     Multiplier Gradient Descent Algorithm
     '''
     x0, y0, lam0 = init
-    T, K, alpha, N_inner = params
+    T, alpha, N_inner = params
     A, B, c = problem_data
     params_gda = [N_inner, alpha, alpha]
     xh, yh, lamh = [x0], [y0], [lam0]
     for r in range(0, T):
         init_r = [xh[r], yh[r], lamh[r]]
-        xgda, ygda, lam_gda = gda(init_r, params_gda, problem_data)
+        xgda, ygda, lam_gda = gda(problem, init_r, params_gda, problem_data)
         xn = xgda[-1]
         yn = ygda[-1]
         lamn = problem.proj_pos(lamh[r] - alpha*(-A*xn - B*yn + c))
@@ -35,15 +36,43 @@ def gda(problem, init, params, problem_data):
 
     xh, yh, lamh = [x0], [y0], [lam0]
     for r in range(0, T):
-        yn = problem.projY(yh[r] + beta*(problem.gradf_y(x[r], y[r]) - np.dot(B.T, lamh[r])))
-        xn = problem.projX(xh[r] - alpha*(problem.gradf_x(x[r], y[r+1]) + np.dot(A.T, lamh[r])))
+        if isinstance(B, (np.ndarray)):
+            yn = problem.projY(yh[r] + beta*(problem.gradf_y(xh[r], yh[r]) - np.dot(B.T, lamh[r])))
+            xn = problem.projX(xh[r] - alpha*(problem.gradf_x(xh[r], yn) + np.dot(A.T, lamh[r])))
+        else:
+            yn = problem.projY(yh[r] + beta*(problem.gradf_y(xh[r], yh[r]) - B*lamh[r]))
+            xn = problem.projX(xh[r] - alpha*(problem.gradf_x(xh[r], yn) + A*lamh[r]))
         lamn = problem.proj_pos(lamh[r] - alpha*(-A*xh[r] - B*yn + c))
         xh.append(xn)
         yh.append(yn)
         lamh.append(lamn)
     return xh, yh, lamh
 
+def plot_conv(xh, yh, lamh):
+    fig1, ax1 = plt.subplots(3)
+    fig1.suptitle("mgd")
+    ax1[0].set_xlabel("iter")
+    ax1[0].set_ylabel("x")
+    ax1[0].set_ylim([-1,1])
+    ax1[0].plot(xh, 'r')
+
+    ax1[1].set_xlabel("iter")
+    ax1[1].set_ylabel("y")
+    ax1[1].plot(yh, 'r')
+    ax1[1].set_ylim([-2,0])
+
+    ax1[2].set_xlabel("iter")
+    ax1[2].set_ylabel("lambda")
+    lam0 = [lamh[i][0] for i in range(len(lamh))]
+    lam1 = [lamh[i][1] for i in range(len(lamh))]
+    ax1[2].plot(lam0, 'r', label="lam[0]")
+    ax1[2].plot(lam1, 'k', label="lam[1]")
+    ax1[2].legend()
+    plt.savefig("debugging_imgs/mgd_toy_example.png")
+    plt.show()
+
 if __name__=='__main__':
-    problem = ToyProb() # From paper on coupled constraints
+    problem = ToyProb(x0 = 0.75, y0 = -0.25, lam0 = np.array([[0],[0]], dtype=object,)) # From paper on coupled constraints
     init, params, problem_data = problem.define()
     xh, yh, lamh = mgd(problem, init, params, problem_data)
+    plot_conv(xh, yh, lamh)
